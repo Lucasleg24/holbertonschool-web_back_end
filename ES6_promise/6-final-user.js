5263555c25aba51fe0934c08c95f917a24c91b46
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-11-20 09:58:58 +0100

    add t6 with fix and test

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: 'fulfilled',
+          value: result.value,
+        };
+      }
+      return {
+        status: 'rejected',
+        value: result.reason,
+      };
+    }));
+}
