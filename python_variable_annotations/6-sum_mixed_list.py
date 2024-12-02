ab2279c78ef1b7df8d452634e88db9082f88161d
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-12-02 13:45:20 +0100

    add t6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+module 6-sum_mixed_list
+"""
+
+
+from typing import List, Union
+
+"""
+import module typing
+"""
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    def sum mixed list with union
+    """
+    return sum(mxd_lst)
