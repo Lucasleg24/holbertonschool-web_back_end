d511005e462bfcae3baa8fae50e104d88b274605
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-12-02 14:03:16 +0100

    add t9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,18 @@
+#!/usr/bin/ env python3
+
+"""
+module 9-element_length
+"""
+
+
+from typing import Iterable, Sequence, List, Tuple
+"""
+import typing
+"""
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    def for length (sequence)
+    """
+    return [(i, len(i)) for i in lst]
