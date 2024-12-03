41cfef5c084f729c3119ad11b77fc1c20a1d2250
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-12-03 11:13:22 +0100

    test dégager les patates

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -17,18 +17,18 @@ import time asyncio and wait_n
 def measure_time(n: int, max_delay: int) -> float:
     """
     Mesure le temps total nécessaire pour exécuter wait_n(n, max_delay)
-    et retourne le temps moyen de cuisson des pommes de terre.
+    et retourne le temps moyen
 
     Args:
         n (int): Nombre d'exécutions de wait_random.
-        max_delay (int): Le délai maximum de cuisson d'une pomme de terre.
+        max_delay (int): Le délai maximum
 
     Returns:
-        float: Temps moyen par cuisson d'une patate avant de couper les échalottes.
+        float: Temps moyen
     """
     start_time = time.perf_counter()  # Démarrer le chronomètre
     asyncio.run(wait_n(n, max_delay))  # Exécuter wait_n de manière asynchrone
-    end_time = time.perf_counter()  # Arrêter le chronomètre et sortir les patates de l'eau
+    end_time = time.perf_counter()  # Arrêter le chronomètre
 
-    total_time = end_time - start_time  # Calculer le temps tota0l de préparation du plat avant service 
+    total_time = end_time - start_time  # Calculer le temps total avant service
     return total_time / n  # Retourner le temps moyen
--- python_async_function/README.md
+++ python_async_function/README.md
@@ -1,8 +1,1 @@
 README.md
-
-Pour une bonne purée :
-
-# # Des patates cuisson vapeur
-
-Les éplucher (ou pas) et les laisser cuire environ 45 minutes, départ à froid. 
-
