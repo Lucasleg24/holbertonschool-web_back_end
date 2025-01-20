aa39d99d74bdec4a283c28f90fca4cfb81193c5e
Lucasleg24 <lucaslegrand.pro@gmail.com>
2025-01-20 15:32:18 +0100

    ad eslint t1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,11 +3,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Création d'un écouteur pour l'entrée standard
 process.stdin.on('data', (data) => {
-    // Affichage du nom saisi
-    process.stdout.write(`Your name is: ${data}`);
+  // Affichage du nom saisi
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 // Écouteur pour détecter la fin de l'entrée (Ctrl+D ou pipe)
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
