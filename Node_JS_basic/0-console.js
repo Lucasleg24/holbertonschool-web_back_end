fe68ec11c77854781721dffbc7c5d387a6867c19
Lucasleg24 <lucaslegrand.pro@gmail.com>
2025-01-20 10:01:42 +0100

    eslint fix

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 // Définition de la fonction
 function displayMessage(message) {
-    console.log(message); // Affiche le message
+  console.log(message); // Affiche le message
 }
 
 // Export de la fonction
